96c515299e09e5e553c9cc8a185448cf216f6ed5
treefroghj-dev <treefroghj@gmail.com>
2026-01-26 18:42:03 +0000

    terraform setup

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "7.16.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "terraform-setup-485500"
+  region      = "us-central1"
+}
+
+resource "google_storage_bucket" "data-bucket" {
+  name          = "terraform-setup-485500-data-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
